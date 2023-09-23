b5f749e27c9c0ac992faf09adda8ba922c7ec612
andriySad <waterpolomen2@gmail.com>
2023-09-23 13:44:38 +0200

    updated constants

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,4 @@
-const kBaseUrl = 'api.openweathermap.org/data/2.5';
-const kWeatherEndpoint = '/weather';
+const kBaseUrl = 'api.openweathermap.org';
+const kWeatherEndpoint = '/data/2.5/weather';
+const kCitiesEndpoint = '/data/2.5/find';
 const kApiKey = '429a47878356dcb48b0041b7cedfe6fa';
