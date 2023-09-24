91a9b56d2a8658a5ddb5435fe6f6030d0f4583f7
andriySad <waterpolomen2@gmail.com>
2023-09-24 20:27:09 +0200

    added popularCity model

---
+++ lib/core/models/popular_city.dart
@@ -0,0 +1,10 @@
+class PopularCity {
+  PopularCity({
+    required this.id,
+    required this.cityName,
+    required this.backgroundImageName,
+  });
+  final int id;
+  final String cityName;
+  final String backgroundImageName;
+}
