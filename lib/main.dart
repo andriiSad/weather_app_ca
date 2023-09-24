3ccf3396e03d9ec89ff00de09a80262176b6a462
andriySad <waterpolomen2@gmail.com>
2023-09-24 13:45:27 +0200

    DI init

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_ca/core/res/colors.dart';
+import 'package:weather_app_ca/core/res/fonts.dart';
+import 'package:weather_app_ca/core/services/injection_container/injection_container.dart';
+import 'package:weather_app_ca/src/weather/presentation/bloc/weather_bloc.dart';
+import 'package:weather_app_ca/src/weather/presentation/screens/home/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //dependencies injection
+  await init();
+
   runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-      debugShowCheckedModeBanner: false,
-    ),
+    const MainApp(),
   );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+    return BlocProvider(
+      create: (context) => serviceLocator<WeatherBloc>(),
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: Fonts.aeonik,
+          appBarTheme: const AppBarTheme(
+            color: Colors.transparent,
+            elevation: 0,
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: Colours.primaryColour,
+          ),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
