79864eb30e91db3eb03a905e9d13a6b99635e7af
andriySad <waterpolomen2@gmail.com>
2023-09-24 20:29:30 +0200

    added string constants

---
+++ lib/core/res/strings.dart
@@ -0,0 +1,4 @@
+const String myNameSurname = 'Andrii Sadovyi';
+const String appName = 'Weather App';
+const String popularCitiesSectionName =
+    'Check the weather in most popular cities in the world';
