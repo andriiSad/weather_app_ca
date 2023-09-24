94afb085aced4cd7fd97c2718e8cade423b752bc
andriySad <waterpolomen2@gmail.com>
2023-09-24 20:25:27 +0200

    added extensions

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 extension ContextExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
 
+  TextTheme get textTheme => theme.textTheme;
+
   MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  ScaffoldMessengerState get scaffoldMessenger => ScaffoldMessenger.of(this);
 
   Size get screenSize => mediaQuery.size;
   double get screenWidth => screenSize.width;
