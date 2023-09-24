f617ad62b3da13e921582cfdb877dcae0364cbc7
andriySad <waterpolomen2@gmail.com>
2023-09-24 20:32:04 +0200

    refactored main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather_app_ca/core/res/colors.dart';
-import 'package:weather_app_ca/core/res/fonts.dart';
+import 'package:weather_app_ca/core/services/bloc_observer.dart';
 import 'package:weather_app_ca/core/services/injection_container/injection_container.dart';
+import 'package:weather_app_ca/core/theme/theme.dart';
 import 'package:weather_app_ca/src/weather/presentation/bloc/weather_bloc.dart';
 import 'package:weather_app_ca/src/weather/presentation/screens/home/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Bloc.observer = const AppBlocObserver();
 
   //dependencies injection
   await init();
@@ -25,18 +27,7 @@ class MainApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => serviceLocator<WeatherBloc>(),
       child: MaterialApp(
-        theme: ThemeData(
-          fontFamily: Fonts.aeonik,
-          appBarTheme: const AppBarTheme(
-            color: Colors.transparent,
-            elevation: 0,
-          ),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          colorScheme: ColorScheme.fromSwatch(
-            accentColor: Colours.primaryColour,
-          ),
-          useMaterial3: true,
-        ),
+        theme: AppTheme.lightTheme,
         home: const HomeScreen(),
         debugShowCheckedModeBanner: false,
       ),
