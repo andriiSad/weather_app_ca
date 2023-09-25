2ada52b71343ee1966b26d832cde219d4f2383a0
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-09-25 15:26:01 +0200

    remoced api key from constants

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,3 @@
 const kBaseUrl = 'api.openweathermap.org';
 const kWeatherEndpoint = '/data/2.5/weather';
 const kCitiesEndpoint = '/data/2.5/find';
-const kApiKey = '429a47878356dcb48b0041b7cedfe6fa';
