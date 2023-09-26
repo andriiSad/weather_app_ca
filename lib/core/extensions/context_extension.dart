56541dc5b08959653f8f5248e293385f80af1d7a
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-09-26 13:46:44 +0200

    added context extensions

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_ca/core/common/providers/location_provider.dart';
+import 'package:weather_app_ca/core/common/providers/theme_provider.dart';
 
 extension ContextExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
@@ -8,6 +11,9 @@ extension ContextExtension on BuildContext {
   MediaQueryData get mediaQuery => MediaQuery.of(this);
 
   ScaffoldMessengerState get scaffoldMessenger => ScaffoldMessenger.of(this);
+
+  LocationProvider get locationProvider => read<LocationProvider>();
+  ThemeProvider get themeProvider => read<ThemeProvider>();
 
   Size get screenSize => mediaQuery.size;
   double get screenWidth => screenSize.width;
